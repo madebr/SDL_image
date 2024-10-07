ce133f1aa0c6b15d152d9196aa323a7c9c7257dc
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-10-07 04:29:08 +0200

    Add parametrized build script
    [ci skip]

--- VisualC/pkg-support/cmake/SDL3_imageConfig.cmake
+++ VisualC/pkg-support/cmake/SDL3_imageConfig.cmake
@@ -7,7 +7,7 @@ set_package_properties(SDL3_image PROPERTIES
     DESCRIPTION "SDL_image is an image file loading library"
 )
 
-cmake_minimum_required(VERSION 3.0)
+cmake_minimum_required(VERSION 3.0...3.5)
 
 set(SDL3_image_FOUND TRUE)
 
@@ -38,12 +38,13 @@ set(SDLIMAGE_BACKEND_IMAGEIO   FALSE)
 set(SDLIMAGE_BACKEND_STB       TRUE)
 set(SDLIMAGE_BACKEND_WIC       FALSE)
 
-if(CMAKE_SIZEOF_VOID_P STREQUAL "4")
+if(SDL_CPU_X86)
     set(_sdl_arch_subdir "x86")
-elseif(CMAKE_SIZEOF_VOID_P STREQUAL "8")
+elseif(SDL_CPU_X64 OR SDL_CPU_ARM64EC)
     set(_sdl_arch_subdir "x64")
+elseif(SDL_CPU_ARM64)
+    set(_sdl_arch_subdir "arm64")
 else()
-    unset(_sdl_arch_subdir)
     set(SDL3_image_FOUND FALSE)
     return()
 endif()
@@ -55,9 +56,9 @@ set(_sdl3image_dll          "${CMAKE_CURRENT_LIST_DIR}/../lib/${_sdl_arch_subdir
 # All targets are created, even when some might not be requested though COMPONENTS.
 # This is done for compatibility with CMake generated SDL3_image-target.cmake files.
 
-if(NOT TARGET SDL3_image::SDL3_image)
-    add_library(SDL3_image::SDL3_image SHARED IMPORTED)
-    set_target_properties(SDL3_image::SDL3_image
+if(NOT TARGET SDL3_image::SDL3_image-shared)
+    add_library(SDL3_image::SDL3_image-shared SHARED IMPORTED)
+    set_target_properties(SDL3_image::SDL3_image-shared
         PROPERTIES
             INTERFACE_INCLUDE_DIRECTORIES "${_sdl3image_incdir}"
             IMPORTED_IMPLIB "${_sdl3image_library}"
@@ -65,6 +66,23 @@ if(NOT TARGET SDL3_image::SDL3_image)
             COMPATIBLE_INTERFACE_BOOL "SDL3_SHARED"
             INTERFACE_SDL3_SHARED "ON"
     )
+endif()
+
+function(_sdl_create_target_alias_compat NEW_TARGET TARGET)
+    if(CMAKE_VERSION VERSION_LESS "3.18")
+        # Aliasing local targets is not supported on CMake < 3.18, so make it global.
+        add_library(${NEW_TARGET} INTERFACE IMPORTED)
+        set_target_properties(${NEW_TARGET} PROPERTIES INTERFACE_LINK_LIBRARIES "${TARGET}")
+    else()
+        add_library(${NEW_TARGET} ALIAS ${TARGET})
+    endif()
+endfunction()
+
+# Make sure SDL3_image::SDL3_image always exists
+if(NOT TARGET SDL3_image::SDL3_image)
+    if(TARGET SDL3_image::SDL3_image-shared)
+        _sdl_create_target_alias_compat(SDL3_image::SDL3_image SDL3_image::SDL3_image-shared)
+    endif()
 endif()
 
 unset(_sdl_arch_subdir)
